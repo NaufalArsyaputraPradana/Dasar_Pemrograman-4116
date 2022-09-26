0bf5c1434939490a0e37af982d68ca0592de62bb
NaufalArsyaputraPradana <111202214606@mhs.dinus.ac.id>
2022-09-27 00:44:07 +0700

    Praktikum Minggu 1

---
+++ Biodata/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Tugas 1 Output Biodata
+
+    cout << "Hello world!\n" << endl;
+
+    cout << "Nama : Naufal Arsyaputra Pradana" << endl;
+    cout << "NIM : A11.2022.14606" << endl;
+    cout << "Progdi : Teknik Informatika" << endl;
+    cout << "Fakultas : Ilmu Komputer" << endl;
+    cout << "Akun Mhs : 111202214606@mhs.dinus.ac.id" << endl;
+    cout << "Alamat Asal : Kecapi, RT 28/05, Jepara" << endl;
+    cout << "sal Sekolah : SMK N 3 Jepara" << endl;
+    cout << "Jurusan : Multimedia" << endl;
+    cout << "Hobi : Musik\n";
+
+    //Hasil Output
+    /*
+    Hello World !
+
+    Nama : Naufal Arsyaputra Pradana
+    NIM : A11.2022.14606
+    Progdi : Teknik Informatika
+    Fakultas : Ilmu Komputer
+    Akun Mhs : 111202214606@mhs.dinus.ac.id
+    Alamat Asal : Kecapi, RT 28/05, Jepara
+    Asal Sekolah : SMK N 3 Jepara
+    Jurusan : Multimedia
+    Hobi : Musik
+    */
+
+    return 0;
+}
