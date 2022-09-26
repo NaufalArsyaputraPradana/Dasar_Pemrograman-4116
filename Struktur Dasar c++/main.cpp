9d9b0a82c4d7fcd16220c37caf4888930e4fcbee
NaufalArsyaputraPradana <111202214606@mhs.dinus.ac.id>
2022-09-27 00:52:33 +0700

    Pengenalan c++ Minggu 1

---
+++ Struktur Dasar c++/main.cpp
@@ -0,0 +1,17 @@
+//Bagian Deklarasi include : mendefinisikan library (pustaka)
+#include <iostream>
+
+//Bagian Deklarasi namespace std : menggunakan namespace std. Karena fungsi-fungsi pada iostream dibungkus dalam namespace std.
+using namespace std;
+
+//Bagian Fungsi : Fungsi main() adalah fungsi yang akan dieksekusi pertamakali saat program dibuka.
+int main()
+
+//Statemen dan ekspresi adalah perintah-perintah yang ditulis di dalam fungsi.
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
+//Comment
+/*Komentar adalah teks yang akan di abaikan oleh komputer*/
