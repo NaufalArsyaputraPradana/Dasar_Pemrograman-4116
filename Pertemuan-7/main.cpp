4aca2ac8a3b746ae7010b7333d36f3c59ae4e453
NaufalArsyaputraPradana <111202214606@mhs.dinus.ac.id>
2022-10-04 16:08:49 +0700

    if else

---
+++ Pertemuan-7/main.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   //if percabangan
+
+   /*int a = 5;
+
+   //if <condition> then <action> endif
+   if (a <= 5 )//bisa true atau false
+   {
+       cout << "Bilangan Kurang Dari 5" << endl;
+   }*/
+
+   /*
+   int x;
+
+   cout << "Masukkan Angka : " ;
+   cin >> x;
+   if ( x%2 == 1 )
+   {
+       cout << "Bilangan Ganjil" << endl;
+   }
+   if (x%2 == 0)
+   {
+       cout << "Bilangan Genap" << endl;
+   }
+   */
+
+
+   /*int x;
+
+   cout << "Masukkan Angka Bilangan Genap yang Lebih Dari 20 : " ;
+   cin >> x;
+   if (x%2 == 0 && x > 20)
+   {
+       cout << "Benar" << endl;
+   }
+   else if (x%2 == 0 && x == 20)
+   {
+       cout << "Benar" << endl;
+   }
+   else
+   {
+       cout << "Salah" << endl;
+   }
+   cout << endl;
+*/
+
+
+/*
+   cout << "Masukkan Angka Bilangan Genap yang Lebih Dari 20 tapi kurang dari 50\natau bilangan ganjil kurang dari 15: " ;
+   cin >> x;
+   if ( (x%2 == 0 && x > 20 && x < 50) || (x%2 == 1 && x <15) )
+   {
+       cout << "Benar" << endl;
+   }
+   else
+   {
+       cout << "Salah" << endl;
+   }
+   cout << endl;
+
+
+
+
+
+
+
+   cout << "Masukkan Angka Bilangan Ganjil Lebih dari 5 dan kurang dari 13 : " ;
+   cin >> x;
+   if ( x%2 == 1 && x > 5 && x < 13)
+   {
+       cout << "Benar" << endl;
+   }
+   else
+   {
+       cout << "Salah" << endl;
+       cout << "Masukkan Angka Bilangan Ganjil Lebih dari 5 dan kurang dari 13 : " ;
+       cin >> x;
+       if ( x%2 == 1 && x > 5 && x < 13)
+       {
+       cout << "Benar" << endl;
+       }
+       else
+       {
+       cout << "Salah" << endl;
+       }
+   }
+   cout << endl;
+*/
+
+
+
+
+
+
+
+    return 0;
+}
