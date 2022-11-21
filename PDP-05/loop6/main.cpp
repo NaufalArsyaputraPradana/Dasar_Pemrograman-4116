55b60c862be15dc66b24b8ea23c7bc88cd60802f
NaufalArsyaputraPradana <111202214606@mhs.dinus.ac.id>
2022-11-22 02:09:10 +0700

    Tugas C++ Sabtu

--- PDP-05/loop6/main.cpp
+++ PDP-05/loop6/main.cpp
@@ -3,33 +3,36 @@ using namespace std;
 
 int main()
 {
-    int x, max, min;
+    int x, max, min; //kamus
 
-    cout << "Program Ini Akan Berhenti Jika Anda Menginput Angka -99\n" << endl;
+    cout << "Program Ini Akan Berhenti Jika Anda Menginput Angka -99\n" << endl; //petunjuk
 
-    while (true)
+    while (true)//looping
     {
+        //input
         cout << "Masukkan Nilai Anda : ";
         cin >> x;
 
+        //proses looping akan berhenti ketika x = -99
          if (x == -99)
         {
             break;
         }
 
-        if (x != -99)
+        if (x != -99)//-99 tidak terhitung dalam operasi mencari max - min
         {
-            if (max < x)
+            if (max < x)//mencari nilai max
             {
                 max = x;
             }
-            if (min > x)
+            if (min > x)//mencari nilai min
             {
                 min = x;
             }
         }
     }
 
+    //output
     cout << "\nMax = " << max << endl;
     cout << "Min = " << min << endl;
 
